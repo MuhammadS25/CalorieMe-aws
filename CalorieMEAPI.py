c4f8444876bc0131927936f99d9189a53efbce30
MuhammadS25 <mohammed20191700542@cis.asu.edu.eg>
2023-04-03 22:51:13 +0200

    handling exceptions

--- CalorieMEAPI.py
+++ CalorieMEAPI.py
@@ -10,16 +10,20 @@ def hello_world():
 
 @app.route('/CalorieMe', methods=['GET','POST'])
 def predict():
-    if request.method == 'POST':
-        img_link = request.form['img_link']
-        img_pixels = request.form['img_pixels']
+    try:
+        if request.method == 'POST':
+            img_link = request.form['img_link']
+            img_pixels = request.form['img_pixels']
 
-        print(img_link, img_pixels)
+            print(img_link, img_pixels)
 
-        label = Test.getFoodWeight(img_link, img_pixels)
-        json = CaloriesEstimation.getCalories(label)
-        return jsonify(json)
-        return jsonify({'msg': 'success', 'size': [img_link, img_pixels]})
+            label = Test.getFoodWeight(img_link, img_pixels)
+            json = CaloriesEstimation.getCalories(label)
+            return jsonify(json)
+            return jsonify({'msg': 'success', 'size': [img_link, img_pixels]})
+    
+    except Exception as e:
+        return jsonify({'msg': 'error', 'error': str(e)})
 
 
 @app.route('/test', methods=['POST'])
