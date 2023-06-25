157e84e66ab48d974c8ec98acd1566720a72bc97
MuhammadS25 <mohammed20191700542@cis.asu.edu.eg>
2023-06-25 16:52:33 +0300

    [Added] Macros values
    Calculating total protien,fat and carbs

--- CaloriesEstimation.py
+++ CaloriesEstimation.py
@@ -12,7 +12,7 @@ def getCalories(labels):
     labels = {key.lower(): value for key, value in labels.items()}
 
     cal_map = {}
-    total_calories = 0
+    total_calories, total_protein, total_fat, total_carb = 0, 0, 0, 0
     for label in labels.keys():
         calories_series = data.loc[data['Food'] == label, 'Calories']
         if calories_series.empty:
@@ -29,6 +29,23 @@ def getCalories(labels):
         total_calories += estimated_calories
         cal_map[label] = estimated_calories
 
+        protein_series = data.loc[data['Food'] == label, 'Protein (g)']
+        protein = float(protein_series.values[0])
+
+        fat_series = data.loc[data['Food'] == label, 'Fat (g)']
+        fat = float(fat_series.values[0])
+
+        carb_series = data.loc[data['Food'] == label, 'Carbohydrates (g)']
+        carb = float(carb_series.values[0])
+
+        estimated_protein = protein * labels[label] / serving
+        estimated_fat = fat * labels[label] / serving
+        estimated_carb = carb * labels[label] / serving
+
+        total_protein += estimated_protein
+        total_fat += estimated_fat
+        total_carb += estimated_carb
+
 
     # print estimated_calories with 1 decimal places
     print(f'Estimated calories: {total_calories:.1f}')
@@ -36,5 +53,9 @@ def getCalories(labels):
     json = {}
     for key, value in cal_map.items():
         json[key] = int(value)
+
+    json['total_protein'] = float(total_protein)
+    json['total_fat'] = float(total_fat)
+    json['total_carb'] = float(total_carb)
 
     return json
