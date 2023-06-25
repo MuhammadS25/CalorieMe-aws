0365c5f815f3f0ed179e086ae185631aeeac1e7c
MuhammadS25 <mohammed20191700542@cis.asu.edu.eg>
2023-06-25 16:32:54 +0300

    [Added] API runs HTTPS

--- CalorieMEAPI.py
+++ CalorieMEAPI.py
@@ -74,4 +74,4 @@ def refresh():
 if __name__ == '__main__':
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=5000)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc', debug=True)
